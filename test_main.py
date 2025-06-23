1183608ff52594ef47580a4d288f0899530f4436
Deepesh <salvedeepesh619@gmail.com>
2025-06-23 18:08:58 +0530

    final commit - all test cases passed successfully

--- test_main.py
+++ test_main.py
@@ -3,3 +3,22 @@ import pytest
 
 def test_empty_string():
     assert add("") == 0
+
+def test_single_number():
+    assert add("1") == 1
+
+def test_two_numbers():
+    assert add("1,2") == 3
+
+def test_multiple_numbers():
+    assert add("1,2,3,4") == 10
+
+def test_newline_separator():
+    assert add("1\n2,3") == 6
+
+def test_custom_delimiter():
+    assert add("//;\n1;2") == 3
+
+def test_negative_numbers():
+    with pytest.raises(Exception, match="negative numbers not allowed: -2, -3"):
+        add("1,-2,-3")
